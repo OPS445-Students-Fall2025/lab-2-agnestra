f8b5c28e250043b7b7452e844d00c8811a72c538
Agnestra Mahat <amahat1@myseneca.ca>
2025-09-16 16:23:12 -0400

    Added lab2b with comments

--- lab2b.py
+++ lab2b.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+# This asks the user input data for the variables name and age
 name = input('Name: ')
 age = input('Age: ')
 
+#This gives you the required output after you input the name and age
 print('Hi ' + name + ', you are ' + str(age) + ' years old.') 
