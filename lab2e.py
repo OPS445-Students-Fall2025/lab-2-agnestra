ed7b71150bc91f3e5186c21ac9425fd609ecebfe
Agnestra Mahat <amahat1@myseneca.ca>
2025-09-18 20:15:39 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+ lab2e: while loops
+'''
+#Author: Agnestra Mahat
+#Author ID: 128939238
+#Date Created: 2025/09/16
+
+count = 10
+
+#Subtracts the count value by 1 until count is not equal to zero
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
